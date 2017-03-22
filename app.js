2feae45ed99916a93eab1e32e303b5df95367333
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-03-22 09:43:33 +0100

    adding edit function and separting function for displaying all from individual display

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var blogForm = document.getElementById('form-edit');
 var blogTitle = document.getElementById('blog-title');
 var txtArea = document.getElementById('text-area');
 var displayArea = document.querySelector('#display');
-var editButton = document.querySelector('#editButton')
+var ListDisplayArea = document.getElementById('list-display')
+var editButton = document.querySelector('#editButton');
+var submitButton = document.getElementById('form-submit');
 var storeItems = JSON.parse(localStorage.getItem('storeItems')) || [];
 var postKeys = JSON.parse(localStorage.getItem('postKeys')) || [];
 var currentDiv = postKeys.length;
+console.log(currentDiv)
 // var storeItems = []
 
 
@@ -22,7 +25,8 @@ blogForm.addEventListener('submit', function(event){
   obj.title = blogTitle.value;
   obj.body = txtArea.value;
 
-  
+  currentDiv = postKeys.length;
+
   storeItems.push(obj);
   console.log(storeItems);
 
@@ -30,7 +34,7 @@ blogForm.addEventListener('submit', function(event){
   postKeys.push(postKey);
   localStorage.setItem('postKeys', JSON.stringify(postKeys));
 
-  populateDisplay(storeItems, displayArea);
+ populateListDisplay(storeItems, ListDisplayArea);
   localStorage.setItem('storeItems', JSON.stringify(storeItems));
   blogTitle.value = '';
   txtArea.value = '';
@@ -38,7 +42,7 @@ blogForm.addEventListener('submit', function(event){
   
 function populateDisplay(display = [], displayParagraph){
   
-  for(var i = 0; i < storeItems.length; i++){
+  for(var i = 0; i < display.length; i++){
     displayParagraph.innerHTML = 
   `
  
@@ -46,16 +50,117 @@ function populateDisplay(display = [], displayParagraph){
       ${storeItems[i].title}
     </h1>
     
-    <div>
+    <p>
       ${storeItems[i].body}
-    </div>
+    </p>
 
     `
   }
   
 }
+
+
+function populateListDisplay(display = [], displayParagraph){
+  displayParagraph.innerHTML = display.map(function(item){
+    return `
+    <h1>
+      ${item.title}
+    </h1>
+    
+    <p>
+      ${item.body}
+    </p>
+    `
+  }).join('');
+}
+
+
+
 function  generateKey() {
   return postKeys.length
 }
 
+
 populateDisplay(storeItems, displayArea);
+
+populateListDisplay(storeItems, ListDisplayArea);
+
+
+editButton.addEventListener('click', function(event){
+  event.preventDefault();
+  blogTitle.value = JSON.parse(localStorage.getItem('storeItems'))[currentDiv].title;
+  txtArea.value = JSON.parse(localStorage.getItem('storeItems'))[currentDiv].body;
+
+  submitButton.addEventListener('click', function(){
+    event.preventDefault();
+
+    blogTitle.value = JSON.parse(localStorage.getItem('storeItems'))[currentDiv].title;
+    txtArea.value = JSON.parse(localStorage.getItem('storeItems'))[currentDiv].body;
+
+  })
+
+})
+
+
+
+
+nextButton.addEventListener('click', function(event){
+  paginateNext();
+});
+
+previousButton.addEventListener('click', function(event){
+  paginatePrevious();
+});
+
+function generateKey() {
+  return postKeys.length;
+}
+
+function getPostForPage(pageId){
+  return JSON.parse(fetchFromLocalStorage(pageId));
+}
+
+function persistToLocalStorage(key,object){
+  localStorage.setItem(key, object);
+}
+
+function fetchFromLocalStorage(key){
+  return localStorage.getItem(key);
+}
+
+function getPostHtml(postData){
+   return `
+
+    <h1>
+      ${postData.title}
+    </h1>
+    
+    <div>
+      ${postData.body}
+    </div>
+
+    `;
+}
+
+function paginateNext(){
+  if(pageId <= postKeys.length - 1){
+    populateDisplay(++currentPage);
+  }
+}
+
+function paginatePrevious(pageId){
+  if(pageId <= 0){
+    populateDisplay(--currentPage);
+  }
+}
+
+
+  
+function populateDisplay(pageId){
+  const pageData = getPostForPage(pageId);
+  console.dir(pageData)
+  if(pageData){
+    displayArea.innerHTML = getPostHtml(pageData);
+  }
+}
+
